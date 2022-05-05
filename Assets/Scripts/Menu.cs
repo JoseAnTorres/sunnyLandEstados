f69aeed9e15ebdcc1f266a84b721ea70ea5d444d
Jose1928Death <jose1928death@gmail.com>
2022-05-05 14:54:56 +0200

    asd
    asd

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private Animator prueba;
+    [SerializeField] private Animator prueba1;
     private void Start()
     {
         prueba.SetBool("estaOculto", false);
+        prueba1.SetBool("estaOculto", false);
     }
 
     public void PruebaPulsado()
     {
         prueba.SetBool("estaOculto", true);
+        prueba1.SetBool("estaOculto", true);
     }
 }
